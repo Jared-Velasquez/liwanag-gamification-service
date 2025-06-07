084c63c72f2616a50104c89e3ae983f06e3b699c
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2025-06-07 13:39:27 -0700

    Initial commit

---
+++ src/test/java/com/liwanag/gamification/GamificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liwanag.gamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
