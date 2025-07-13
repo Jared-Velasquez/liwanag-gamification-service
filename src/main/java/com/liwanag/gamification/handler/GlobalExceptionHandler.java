2a85c3b822f547a2b1d9e7788e6dd5a60b7dbcca
Jared Velasquez <jaredvel25@gmail.com>
2025-07-13 15:02:41 -0700

    feature: add global exception handler for illegal argument exceptions

--- src/main/java/com/liwanag/gamification/handler/GlobalExceptionHandler.java
+++ src/main/java/com/liwanag/gamification/handler/GlobalExceptionHandler.java
@@ -1,8 +1,22 @@
 package com.liwanag.gamification.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> illegalArgument(IllegalArgumentException iae) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("error", iae.getMessage());
+        return new ResponseEntity<>(
+                map,
+                HttpStatus.BAD_REQUEST
+        );
+    }
 }
