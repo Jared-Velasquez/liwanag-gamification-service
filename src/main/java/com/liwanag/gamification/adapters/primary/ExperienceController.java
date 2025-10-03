3c81d147a3ca1c0d4f276f13dd5abbabdf635080
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2025-10-03 00:24:14 -0700

    feat: add some endpoints to controllers

--- src/main/java/com/liwanag/gamification/adapters/primary/ExperienceController.java
+++ src/main/java/com/liwanag/gamification/adapters/primary/ExperienceController.java
@@ -1,11 +1,22 @@
 package com.liwanag.gamification.adapters.primary;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/experience")
 @RequiredArgsConstructor
 public final class ExperienceController {
+    @GetMapping("/")
+    public ResponseEntity<?> getExperience(
+            @RequestHeader(name = "x-cognito-sub") UUID userId
+    ) {
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/liwanag/gamification/adapters/primary/LeaderboardController.java
+++ src/main/java/com/liwanag/gamification/adapters/primary/LeaderboardController.java
@@ -1,11 +1,38 @@
 package com.liwanag.gamification.adapters.primary;
 
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/leaderboards")
 @RequiredArgsConstructor
 public final class LeaderboardController {
+    @GetMapping("/levels")
+    public ResponseEntity<?> getTopLevels(
+            @RequestParam(value = "count", defaultValue = "5") @Min(1) int count,
+            @RequestParam(value = "page", defaultValue = "0") @Min(0) int page
+    ) {
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/combo")
+    public ResponseEntity<?> getTopComboStreaks(
+            @RequestParam(value = "count", defaultValue = "5") @Min(1) int count,
+            @RequestParam(value = "page", defaultValue = "0") @Min(0) int page
+    ) {
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/daily")
+    public ResponseEntity<?> getTopDailyPoints(
+            @RequestParam(value = "count", defaultValue = "5") @Min(1) int count,
+            @RequestParam(value = "page", defaultValue = "0") @Min(0) int page
+    ) {
+        return ResponseEntity.noContent().build();
+    }
 }
